30a940936eb40febedaae378b132468734c96136
Tengis Gantulga <tengisgan@vt.edu>
2020-09-29 17:12:49 -0400

    test class documentation

--- src/covid19trackingmanager/tests.java
+++ src/covid19trackingmanager/tests.java
@@ -3,6 +3,14 @@ package covid19trackingmanager;
 import covid19trackingmanager.Covid19TrackingManager;
 import junit.framework.TestCase;
 
+/**
+ * Test class for project 1. 
+ * 
+ * @author Tengis Gantulga and Nikolai Long
+ * @pid tengisgan and nikolai
+ * 
+ * @version 2020.09.29
+ */
 public class tests extends TestCase {
     
 	private Covid19TrackingManager tracker;
