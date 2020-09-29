0e3d50eb9225fc92646553318ced410eaa8d23fc
Nikolai <nikolai@vt.edu>
2020-09-28 21:25:43 -0400

    fixed test file

--- src/covid19trackingmanager/tests.java
+++ src/covid19trackingmanager/tests.java
@@ -2,8 +2,13 @@ package covid19trackingmanager;
 
 import junit.framework.TestCase;
 
-public class Covid19TrackingManager extends TestCase {
-    
+public class tests extends TestCase {
+	private Covid19TrackingManager tracker;
+	
+	public void setUp() {
+		tracker = new Covid19TrackingManager();
+	}
+	
     
     
 }
